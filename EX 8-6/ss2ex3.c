20d2960ac03ae258380875556cb8fa97d8e12a51
Nguyen Trung HIeu <65000911+hieuntth2002015@users.noreply.github.com>
2020-05-08 16:32:34 +0700

    8-6

---
+++ EX 8-6/ss2ex3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+	int a, b, c;
+    printf(" Nhap so a : ");
+    scanf("%d", &a);
+    printf(" Nhap so b : ");
+    scanf("%d", &b);
+    
+    c= a+b;
+    printf(" C = %d", c);
+return 0;
+}
