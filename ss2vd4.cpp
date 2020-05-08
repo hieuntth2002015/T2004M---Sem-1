e6c40ed3f03330e7de9914179f1052af614d741b
Nguyen Trung HIeu <65000911+hieuntth2002015@users.noreply.github.com>
2020-05-08 10:50:36 +0700

    Create ss2vd4.cpp

---
+++ ss2vd4.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+	int x,y;
+	x = 5;
+	y = 2;
+	printf("  2 so nguyen :%d va %d\n", x, y);
+	printf(" Phep cong cua hai so nguyen la :%d\n", x +y);
+	printf(" phep tru cua hai so nguyen la : %d\n", x -y);
+	printf(" Phep nhan cua hai so nguyen la : %d\n", x *y);
+	printf(" Phep chia cua hai so nguyen la : %d\n", x/y);
+	printf(" phep chia cua hai so nguyen co du la :%d", x%y);
+	getchar();
+}
