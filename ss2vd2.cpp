7c2dc2f253d6596eccf504ab3d6e0cee1c5d5969
Nguyen Trung HIeu <65000911+hieuntth2002015@users.noreply.github.com>
2020-05-08 10:46:21 +0700

    Update ss2vd2.cpp

--- ss2vd2.cpp
+++ ss2vd2.cpp
@@ -18,6 +18,7 @@ int main() {
 	
 	printf(" Nhap vao gt c 8bits tu (-128 - 128) : %c\n", c);// gom ca chu//
 	printf(" Nhap vao gt f 32 bits gioi han 6 chu so thap phan: %f\n", f);
+	printf(" Nhap vao gt lf 64 bits gh 10 so thap phan : %lf\n", lf);
 	printf(" Nhap vao gt ld 32-128 bits 10 stp: %ld\n", ld);
 	printf("Nhap vao gt d 2 byte 16 bits -32,768- 32,767 : %hd", hd);
  return 0;
